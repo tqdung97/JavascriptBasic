447a94185c4bb00fa5fd9b8bbf0c25cffb76e26e
sonnguyenyhct <khacsonyhct@gmail.com>
2022-11-25 19:49:22 +0700

    thuc hanh

---
+++ lession3/lession3.js
@@ -0,0 +1,30 @@
+function inputArr(arr) {
+    let arr2 = arr.map(x => x*2);
+    return arr2;
+}
+
+//B3:
+
+function getArr( input) {
+    let arr = [];
+    for(let i = 0; i <= input; i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
+//B5
+
+function bai5( arr) {
+    let sum = 0;
+    for( let i = 0; i < arr.length; i++){
+        sum =+ sum + arr[i];
+    }
+    for (let i = 0; i <= arr.length-1; i++) {
+        if (arr[i] == (sum/2)) {
+            return true;
+        }
+    }
+    return false;
+}
+
