c9ac3420f49246941e07063db7d4146852769f95
sonnguyenyhct <khacsonyhct@gmail.com>
2022-11-25 20:21:48 +0700

    m

--- lession3/lession3.js
+++ lession3/lession3.js
@@ -28,3 +28,31 @@ function bai5( arr) {
     return false;
 }
 
+// Bài 2: Viết một hàm nhận tham số là một mảng gồm các giá trị số. Hàm trả về một mảng mới gồm
+// các giá trị số chẵn có trang mảng tham số truyền vào. 
+function bai2( input) {
+    let arr = [];
+
+    for( let i = 0; i <= input.length; i++) {
+        if(arr[i] % 2 == 0){
+            arr.push(i);
+        }
+        
+    }
+    return arr;
+}
+input = [1, 2, 5, 6];
+bai2(input);
+
+// Bài 4: Viết một hàm tương tự như bài 3 kết quả trả về theo giá trị ngược lại, từ giá trị tham số đến 0
+function bai4( input) {
+    let arr = [0];
+
+    for(let i =0; i < input.length; i++) {
+        if(arr[i] % 2 != 0) {
+            arr.push(i)
+        }
+    }
+    return arr;
+}
+
