e1117200c0b449361a467ae5ba5362c9c28fcbd8
sonnguyenyhct <khacsonyhct@gmail.com>
2022-12-05 20:54:43 +0700

    lab3

--- leanr_day06/lab3/bai1.js
+++ leanr_day06/lab3/bai1.js
@@ -3,6 +3,7 @@
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const content = document.querySelector(".event");
+
 btn1.addEventListener("click", (plusNumber) =>{
     const clickTarget = plusNumber.target;
     if(clickTarget.classList.contains("buttons-group")) {
@@ -17,13 +18,3 @@ btn2.addEventListener("click", (minusNumber) =>{
     }
     content.textContent--;
 });
-// Bài 2: Giả sử cửa sổ trình duyệt của bạn là một căn phòng, nền trắng là đang bật đèn. Hãy tạo một nút button để
-// tắt đèn của căn phòng này. Hay quy định content hoặc style cho nút này để người dùng biết ấn vào sẽ thế nào
-// (Ví dụ đèn đang sáng thì content ghi là “On”, ấn tắt đi thì chuyển sang “Off”). Có thể thêm icon hình bóng đèn và
-// đổi màu nền của button sang màu tối hơn khi đèn tắt.
-// Bài 3: Tạo cửa sổ modal. Tạo một button, khi click button đó thì mở ra một cửa sổ ở vị trí chính giữa cửa sổ tình
-// duyệt và có một nút button để đóng. Click vào nút đó thì sẽ đóng cửa sổ vừa mở ra. (Gợi ý, sử dụng position
-// fixed cho cửa sổ modal).
-// Bài 4 (Nâng cao): Cải tiến bài tập 3. Khi cửa sổ modal mở ra, nền của cả cửa sổ trình duyệt phía sau modal đó
-// có một màu tối có độ trong suốt. Khi click vào nút đóng hay bên ngoài của cửa sổ modal đều đóng được cửa sổ
-// này
--- leanr_day06/lab3/bai2.js
+++ leanr_day06/lab3/bai2.js
@@ -1,1 +1,15 @@
-const main = document.querySelector()
+// Bài 2: Giả sử cửa sổ trình duyệt của bạn là một căn phòng, nền trắng là đang bật đèn. Hãy tạo một nút button để
+// tắt đèn của căn phòng này. Hay quy định content hoặc style cho nút này để người dùng biết ấn vào sẽ thế nào
+// (Ví dụ đèn đang sáng thì content ghi là “On”, ấn tắt đi thì chuyển sang “Off”). Có thể thêm icon hình bóng đèn và
+// đổi màu nền của button sang màu tối hơn khi đèn tắt.
+const button = document.querySelector('.lamp-button');
+const room = document.querySelector('.room');
+button.addEventListener("click", () => {
+    if (room.style.backgroundColor === "white") {
+        room.style.backgroundColor = "black";
+        button.style.color = "black";
+    } else {
+        room.style.backgroundColor = "white";
+        button.style.color = "green";
+    }
+})
